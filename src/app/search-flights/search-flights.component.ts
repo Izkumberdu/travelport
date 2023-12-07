85cceb7304d6d9a860c722a6e53b9b001ec71db5
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2023-12-07 13:58:56 +0800

    Finalized Search flights

--- src/app/search-flights/search-flights.component.ts
+++ src/app/search-flights/search-flights.component.ts
@@ -9,21 +9,74 @@ import { CommonModule } from '@angular/common';
   styleUrl: './search-flights.component.css'
 })
 export class SearchFlightsComponent {
+  totalPassengers: number = 0;
+
 
   ngOnInit() {
-    // Logic for handling the visibility of the Return Date input
-    const typeFlightDropdown = document.getElementById('TypeFlight') as HTMLSelectElement;
-    const returnDateContainer = document.getElementById('returnDateContainer');
+    document.addEventListener("DOMContentLoaded", () => {
+      
+      // Get the type of flight and return date input elements
+      const typeFlight = document.getElementById("TypeFlight") as HTMLSelectElement;
+      const returnDateInput = (document.getElementById("returnDate")?.parentNode?.parentNode as HTMLElement) ?? null;
 
-    if (typeFlightDropdown && returnDateContainer) {
-      typeFlightDropdown.addEventListener('change', () => {
-        if (typeFlightDropdown.value === 'Round-Trip') {
-          returnDateContainer.style.display = 'block';
-        } else {
-          returnDateContainer.style.display = 'none';
+      const handleButtonClick = (action: string, countElementId: string) => {
+        const countElement = document.getElementById(countElementId) as HTMLHeadingElement;
+        let currentValue = parseInt(countElement.innerText);
+  
+        if (action === "increase") {
+          countElement.innerText = (currentValue + 1).toString();
+        } else if (action === "decrease" && currentValue > 0) {
+          countElement.innerText = (currentValue - 1).toString();
         }
-      });
-    }
+        this.updateTotalPassengers();
+      };
+
     
+    
+
+      document.getElementById("btn-increase-children")?.addEventListener("click", function () {
+        handleButtonClick("increase", "numChildren");
+      });
+      document.getElementById("btn-decrease-children")?.addEventListener("click", function () {
+        handleButtonClick("decrease", "numChildren");
+      });
+      document.getElementById("btn-increase-toddler")?.addEventListener("click", function () {
+        handleButtonClick("increase", "numToddlers");
+      });
+      document.getElementById("btn-decrease-toddler")?.addEventListener("click", function () {
+        handleButtonClick("decrease", "numToddlers");
+      });
+      document.getElementById("btn-increase-adults")?.addEventListener("click", function () {
+        handleButtonClick("increase", "numAdults");
+      });
+      
+      document.getElementById("btn-decrease-adults")?.addEventListener("click", function () {
+        handleButtonClick("decrease", "numAdults");
+      });
+
+      function toggleReturnDate() {
+        if (returnDateInput) {
+          returnDateInput.style.display = typeFlight.value === "Round-Trip" ? "block" : "none";
+        }
+        else{
+
+        }
+      }
+    
+
+      toggleReturnDate();
+
+      typeFlight.addEventListener("change", toggleReturnDate);
+    });
   }
+ 
+   updateTotalPassengers() {
+    const numAdults = parseInt(document.getElementById("numAdults")?.innerText || "0", 10);
+    const numChildren = parseInt(document.getElementById("numChildren")?.innerText || "0", 10);
+    const numToddlers = parseInt(document.getElementById("numToddlers")?.innerText || "0", 10);
+  
+    this.totalPassengers = numAdults + numChildren + numToddlers;
+  }
+
 }
+
